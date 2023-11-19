bd5848ac1febd79a99bfe2316480d94fd8ef841d
savina <savina.jabbo@gmail.com>
2023-11-18 20:37:28 -0800

    basic accordion

--- accordion-faq/faq-accordion-card-main/script.js
+++ accordion-faq/faq-accordion-card-main/script.js
@@ -1,1 +1,7 @@
-console.log('test');
+document.querySelectorAll('.accordion__question').forEach((item) => {
+    item.addEventListener('click', (event) => {
+        console.log('click!');
+        item.classList.toggle('open');
+        item.nextElementSibling.classList.toggle('open');
+    });
+});
